c54a7081d60d9a50c9c19ff85ae374ba3e3e7ff6
SammuelPG <palazongarciasamuel@gmail.com>
2023-02-02 09:16:02 +0100

    Paso 1 inicializo y realizo un commit local

---
+++ src/ejerciciogit/EjercicioGit.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejerciciogit;
+
+/**
+ *
+ * @author dam
+ */
+public class EjercicioGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Estoy en el main de master local");
+        System.out.println("He creado el proyecto En estas 2 lineas");
+        System.out.println("Líneas en el método main");
+    }
+    
+}
